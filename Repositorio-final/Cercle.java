ebb7d812f6db33506a5a4c2230372ec7f83bdf7b
Ignacio <ballobarcity@gmail.com>
2018-05-21 17:43:49 +0200

    nombre

--- Repositorio-final/Cercle.java
+++ Repositorio-final/Cercle.java
@@ -27,5 +27,29 @@ public class Cercle {
 
 	  }
 
+//getters and setters
+	 public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+
+
+	 public String toString() {
+	    String tronk = "X: " + this.x + " Y: " + this.y;
+	    return tronk;
+	 }
+	 
+
 
 }
