7240d4c2e2a742c57f4bd8849a5c8fee37681714
Ignacio <ballobarcity@gmail.com>
2018-05-21 18:53:08 +0200

    main

--- Cercle.java
+++ Cercle.java
@@ -59,4 +59,19 @@ public class Cercle {
 	    String tronk = "X: " + this.x + " Y: " + this.y;
 	    return tronk;
 	 }
+
+public static void main(String[] args) {
+		Cercle circulo1 = new Cercle(12,44);	
+		Cercle circulo2 = new Cercle(22,55);
+		Cercle circulo3 = new Cercle(33,44);
+		Cercle circulo4 = new Cercle(22,55);
+		
+		System.out.println(circulo1);
+		System.out.println(circulo2);
+		System.out.println(circulo3);
+		System.out.println(circulo4);
+	 
+
+
+}
 }
