6a6b2222ffd2d1e26232eab45cdf20df8fddb855
Ignacio <ballobarcity@gmail.com>
2018-05-21 18:40:48 +0200

    getter and setter

--- practica-git/Cercle.java
+++ practica-git/Cercle.java
@@ -36,4 +36,28 @@ public Cercle () {
 	     y=yy;     
 
 	  }
+
+//getters and setters
+	 public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+
+
+	 public String toString() {
+	    String tronk = "X: " + this.x + " Y: " + this.y;
+	    return tronk;
+	 }
+
 }
