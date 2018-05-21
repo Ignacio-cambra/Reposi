53299efb9efa3456d6c48bf386aead3887782eec
Ignacio <ballobarcity@gmail.com>
2018-05-21 18:17:49 +0200

    Creacion to string y constructores

--- Nuevo-repo/Cercle.java
+++ Nuevo-repo/Cercle.java
@@ -17,4 +17,20 @@ public class Cercle {
 
 	  }
 
+public Cercle () {
+	     this.x = 4.3f;
+	     this.y = 4.3f;
+	 }
+	 
+
+	public Cercle (Cercle cercle) {
+	     this.x = cercle.x;
+	     this.y = cercle.y;
+	 }
+
+	 public String toString() {
+	    String tronk = "X: " + this.x + " Y: " + this.y;
+	    return tronk;
+	 }
+
 }
